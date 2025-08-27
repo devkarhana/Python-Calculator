08cf97eae6b56602f91ad097b808e041d9b97dbe
Dev Karhana <200189920+devkarhana@users.noreply.github.com>
2025-08-27 12:59:57 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,10 @@
+class Order :
+
+    def __init__(self, item , price):
+        self.item = item
+        self.price = price
+
+order1 =  Order("tea",20)
+order2 = Order("chips", 15)
+print(order2 .price)
+        
